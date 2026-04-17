cccfdcb9d0e7a653f8af261c13b3ef791f04b5b3
Joice Barbosa Santos <125328143+JoiceBsantos@users.noreply.github.com>
2026-04-17 11:28:20 -0300

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,1 +1,14 @@
+import pytest
+from httpx import AsyncClient, ASGITransport
+from main import app
 
+@pytest.mark.asyncio
+async def test_cadastro_usuario():
+    transport = ASGITransport(app=app)
+    async with AsyncClient(transport=transport, base_url="http://test") as ac:
+        resp = await ac.post("/usuarios", json={
+            "nome": "Usuário Teste",
+            "email": "teste@exemplo.com",
+            "senha": "123456"
+        })
+        assert resp.status_code == 200
